6b524620e3b59a4804f9640bece9a496cd52194c
Mike McCoy <michael.b.mccoy@gmail.com>
2020-11-03 06:52:40 -0800

    Finished linting

--- ghostwriter/camera/gamma.py
+++ ghostwriter/camera/gamma.py
@@ -9,6 +9,7 @@ class GammaCorrector:
     """
     Gamma correction using pre-computed lookup table.
     """
+
     _BASE = np.linspace(0, 1, 256)
 
     def __init__(self, num_levels=128, gamma_min=0.001, gamma_max=5.0):
