3746553fa178573212320911754d1f67fa69cb1f
Mike McCoy <michael.b.mccoy@gmail.com>
2020-11-06 23:35:36 -0800

    update

--- ghostwriter/pixel/hello_world.py
+++ ghostwriter/pixel/hello_world.py
@@ -7,12 +7,20 @@ import neopixel
 
 def main():
     pixels = neopixel.NeoPixel(pin.D18, 64)
+    try:
+        run(pixels)
+    finally:
+        for i in range(pixels.n):
+            pixels[0] = [0, 0, 0]
+
+
+def run(pixels):
     pixel_value = [0, 0, 0]
     for i in range(1000):
-        pixel_value = (pixel_value[0] + i, pixel_value[1] + i % 2, pixel_value[2] + i %3)
-        pixels[0] = pixel_value
+        pixel_value = ((pixel_value[0] + i) % 256, (pixel_value[1] + i % 2)% 256, (pixel_value[2] + i %3) % 256)
+        pixels[i] = pixel_value
         time.sleep(0.01)
-        time.sleep(0.01)
+
 
 
 if __name__ == "__main__":
