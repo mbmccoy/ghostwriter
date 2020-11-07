daef61885734ba9333a86b09e9b37c4d45665a71
Mike McCoy <michael.b.mccoy@gmail.com>
2020-11-06 23:28:50 -0800

    Blink

--- ghostwriter/pixel/hello_world.py
+++ ghostwriter/pixel/hello_world.py
@@ -9,8 +9,9 @@ def main():
     pixels = neopixel.NeoPixel(pin.D18, 1)
     for i in range(10):
         pixels[0] = (128, 0, 0)
-        time.sleep(1)
+        time.sleep(0.25)
         pixels[0] = (0, 0, 0)
+        time.sleep(0.25)
 
 
 if __name__ == "__main__":
