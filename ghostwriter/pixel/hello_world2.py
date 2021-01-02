388f808eab7c4b65fb5904ac6fb21762ef5988ea
tcrensink <tcrensink@gmail.com>
2021-01-01 20:12:26 -0800

    lint

--- ghostwriter/pixel/hello_world2.py
+++ ghostwriter/pixel/hello_world2.py
@@ -9,21 +9,22 @@ import time
 import neopixel
 
 # number of LEDs; fewer than available is OK
-N = 16*16
+N = 16 * 16
 # brightness parameter: increase to ~1 if you see strange behavior
 brightness = 1
 # time increment in sec; LEDs response time is not fast and decreases with num LEDs
-inc = .0005
+inc = 0.0005
+
 
 def test():
     pixels = neopixel.NeoPixel(pin.D18, N, brightness=brightness)
     # red to green
     for j in range(255):
-        pixels.fill((255-j,j, 0))
+        pixels.fill((255 - j, j, 0))
         time.sleep(inc)
     # green to blue
     for j in range(255):
-        pixels.fill((0, 255-j,j))
+        pixels.fill((0, 255 - j, j))
         time.sleep(inc)
 
 
