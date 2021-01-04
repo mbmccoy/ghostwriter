1f7121fb5c4d2f79e2974ed7b1db6e6491ca449e
Michael McCoy <michael.b.mccoy@gmail.com>
2021-01-03 22:34:09 -0800

    Fixed bug

--- ghostwriter/pixel/hello_world.py
+++ ghostwriter/pixel/hello_world.py
@@ -27,7 +27,7 @@ def run(display: Display, shape: Tuple[int, ...]):
     n = np.product(shape)
     image = np.zeros((shape[0], shape[1], 3), dtype=int)
     for i, j in product(*map(range, shape)):
-        pixel_value = (100 % 256, (2 * i) % 256, (128 - 2 * j) % 256)
+        pixel_value = (100 % 256, (8 * i) % 256, (128 - 12 * j) % 256)
         image[i, j, :] = pixel_value
         display.show(image)
     run_time = time.time() - start_time
